a1d718054fd5797014f7b19874fb75de6925d920
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2015-04-20 20:05:47 +0200

    Added a few rotation tests.

--- polyominoes/test_transformations.py
+++ polyominoes/test_transformations.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from polyominoes.polyomino import Polyomino
-from polyominoes.transformations import reflect
+from polyominoes.transformations import reflect, rotate
 
 
 class TestReflect(TestCase):
@@ -22,3 +22,23 @@ class TestReflect(TestCase):
         reflected_polyomino = reflect(polyomino)
         expected_polyomino = Polyomino([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
         self.assertEqual(reflected_polyomino, expected_polyomino)
+
+
+class TestRotate(TestCase):
+    def test_all_corners_full(self):
+        polyomino = Polyomino([[1, 0, 1], [0, 0, 0], [1, 0, 1]])
+        rotate_polyomino = rotate(polyomino)
+        expected_polyomino = Polyomino([[1, 0, 1], [0, 0, 0], [1, 0, 1]])
+        self.assertEqual(rotate_polyomino, expected_polyomino)
+
+    def test_diagonal(self):
+        polyomino = Polyomino([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        rotated_polyomino = rotate(polyomino)
+        expected_polyomino = Polyomino([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
+        self.assertEqual(rotated_polyomino, expected_polyomino)
+
+    def test_upper_line(self):
+        polyomino = Polyomino([[1, 1, 1, 1], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+        rotated_polyomino = rotate(polyomino)
+        expected_polyomino = Polyomino([[0, 1, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]])
+        self.assertEqual(rotated_polyomino, expected_polyomino)
