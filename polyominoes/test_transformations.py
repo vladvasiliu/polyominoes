bce8251eee396923826cab190f98f5dd5039a91b
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2015-04-29 18:39:02 +0200

    Added more transformation tests

--- polyominoes/test_transformations.py
+++ polyominoes/test_transformations.py
@@ -17,22 +17,52 @@ class TestReflect(TestCase):
         expected_polyomino = Polyomino({(0, 0), (-1, 1), (-2, 2)})
         self.assertEqual(reflected_polyomino, expected_polyomino)
 
+    def test_all_corners_full_around_origin(self):
+        polyomino = Polyomino({(-5, -5), (5, 5), (-5, 5), (5, -5)})
+        reflected_polyomino = reflect(polyomino)
+        expected_polyomino = Polyomino({(-5, -5), (5, 5), (-5, 5), (5, -5)})
+        self.assertEqual(reflected_polyomino, expected_polyomino)
+
+    def test_L_outside_of_origin(self):
+        polyomino = Polyomino({(1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (2, 2), (3, 2)})
+        reflected_polyomino = reflect(polyomino)
+        expected_polyomino = Polyomino({(-3, 2), (-2, 2), (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2)})
+        self.assertEqual(reflected_polyomino, expected_polyomino)
+
 
 class TestRotate(TestCase):
     def test_all_corners_full(self):
         polyomino = Polyomino({(0, 0), (3, 0), (0, 3), (3, 3)})
         rotate_polyomino = rotate(polyomino)
-        expected_polyomino = Polyomino({(0, 0), (3, 0), (0, 3), (3, 3)})
+        expected_polyomino = Polyomino({(0, 0), (-3, 0), (0, 3), (-3, 3)})
         self.assertEqual(rotate_polyomino, expected_polyomino)
 
     def test_diagonal(self):
         polyomino = Polyomino({(0, 0), (1, 1), (2, 2)})
         rotated_polyomino = rotate(polyomino)
-        expected_polyomino = Polyomino({(2, 0), (1, 1), (0, 2)})
+        expected_polyomino = Polyomino({(0, 0), (-1, 1), (-2, 2)})
         self.assertEqual(rotated_polyomino, expected_polyomino)
 
     def test_upper_line(self):
         polyomino = Polyomino({(0, 0), (1, 0), (2, 0)})
         rotated_polyomino = rotate(polyomino)
-        expected_polyomino = Polyomino({(2, 0), (2, 1), (2, 2)})
+        expected_polyomino = Polyomino({(0, 0), (0, 1), (0, 2)})
+        self.assertEqual(rotated_polyomino, expected_polyomino)
+
+    def test_all_corners_full_around_origin(self):
+        polyomino = Polyomino({(-5, -5), (5, 5), (-5, 5), (5, -5)})
+        rotated_polyomino = rotate(polyomino)
+        expected_polyomino = Polyomino({(-5, -5), (5, 5), (-5, 5), (5, -5)})
+        self.assertEqual(rotated_polyomino, expected_polyomino)
+
+    def test_L_outside_of_origin(self):
+        polyomino = Polyomino({(1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (2, 2), (3, 2)})
+        rotated_polyomino = rotate(polyomino)
+        expected_polyomino = Polyomino({(-2, 1), (-2, 2), (-2, 3), (-1, 1), (0, 1), (1, 1), (2, 1)})
+        self.assertEqual(rotated_polyomino, expected_polyomino)
+
+    def test_L_around_origin(self):
+        polyomino = Polyomino({(-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2), (0, 2), (1, 2), (2, 2)})
+        rotated_polyomino = rotate(polyomino)
+        expected_polyomino = Polyomino({(-2, -1), (-2, 0), (-2, 1), (-2, 2), (-1, -1), (0, -1), (1, -1), (2, -1)})
         self.assertEqual(rotated_polyomino, expected_polyomino)
