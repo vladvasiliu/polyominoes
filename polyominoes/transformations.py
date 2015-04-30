be82a6abd253943f58aed056f036d5ac893725a9
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2015-04-30 08:08:43 +0200

    Added comments and improved code clarity.

--- polyominoes/transformations.py
+++ polyominoes/transformations.py
@@ -21,27 +21,25 @@ def rotations(polyomino):
 
 
 def normalise(polyomino):
+    """ Translates the polyomino so that all coordinates are positive and minimal.
+    """
     container = polyomino.container
 
-    min_x = min(container, key=itemgetter(0))[0]
-    min_y = min(container, key=itemgetter(1))[1]
-
-    delta_x = 0 - min_x
-    delta_y = 0 - min_y
+    delta_x = - min(container, key=itemgetter(0))[0]
+    delta_y = - min(container, key=itemgetter(1))[1]
 
     return translate(polyomino, delta_x, delta_y)
 
 
 def translate(polyomino, delta_x, delta_y):
-    new_container = polyomino.container
-    new_container = {(x + delta_x, y + delta_y) for x, y in new_container}
+    old_container = polyomino.container
+    new_container = {(x + delta_x, y + delta_y) for x, y in old_container}
     polyomino.container = new_container
     return polyomino
 
 
 def rotate(polyomino):
-    """ Rotates the polyomino 90 degrees clockwise.
-        That is: transpose, that reverse each row
+    """ Rotates the polyomino 90 degrees clockwise around (0, 0).
     """
     old_container = polyomino.container
     new_container = {(-y, x) for x, y in old_container}
